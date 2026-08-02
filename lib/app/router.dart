94fb4e96f160d52e77fe0b9b71ca98493b227e3f
Lilian Pletosu <lilian.pletosu@trakosoft.ro>
2026-08-02 23:58:37 +0300

    Implement native splash screen for Android and iOS using new assets and styles

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -2,19 +2,13 @@ import 'package:go_router/go_router.dart';
 
 import '../features/map/presentation/map_page.dart';
 import '../features/profile/presentation/profile_page.dart';
-import '../features/splash/presentation/splash_page.dart';
 
 class AppRouter {
   AppRouter._();
 
   static final GoRouter router = GoRouter(
-    initialLocation: SplashPage.route,
+    initialLocation: MapPage.route,
     routes: [
-      GoRoute(
-        path: SplashPage.route,
-        name: SplashPage.name,
-        builder: (_, __) => const SplashPage(),
-      ),
       GoRoute(
         path: MapPage.route,
         name: MapPage.name,
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app/app.dart';
 
-void main() {
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const ProviderScope(child: MyApp()));
+  await Future.delayed(const Duration(seconds: 2));
+  FlutterNativeSplash.remove();
 }
